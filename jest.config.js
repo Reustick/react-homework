005dc4dc59f979d21b157615baa7082a47171bed
Rustam <rustam391@gmail.com>
2021-08-10 21:56:11 +0300

    исправит гитигнор

--- jest.config.js
+++ jest.config.js
@@ -1,20 +1,11 @@
-// For a detailed explanation regarding each configuration property, visit:
-// https://jestjs.io/docs/en/configuration.html
-
 module.exports = {
   clearMocks: true,
   coverageDirectory: "coverage",
   testEnvironment: "jsdom",
-  // setupFilesAfterEnv: [
-  //   "<rootDir>/internals/jestSettings.js",
-  //   "<rootDir>/internals/jestSetup.ts",
-  // ],
   transform: {
     "^.+\\.(js|jsx|ts|tsx)$": "<rootDir>/node_modules/babel-jest",
-    /*"^.+\\.mdx$": "@storybook/addon-docs/jest-transform-mdx",*/
   },
   moduleNameMapper: {
-    // https://jestjs.io/docs/en/webpack#handling-static-assets
     "\\.(css|less)$": "<rootDir>/internals/__mocks__/styleMock.js",
     "^@/(.*)$": "<rootDir>/src/$1",
   },
